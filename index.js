64ff01cae2a3ba2e6bc8b53a865940d349f3defc
John Mark Redding <johnmarkredding@gmail.com>
2018-10-30 00:27:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  let array = [];
+  for(let i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  let i = 0;
+  while(i < facts.length()) {
+    i++;
+    facts[i] = `${facts[i]}!!!`;
+  }
+  return facts;
+}
